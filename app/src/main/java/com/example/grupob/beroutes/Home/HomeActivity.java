e9765d86bb51f7f38c9f72bbe5fe5f6cf74ff711
kubalaCEV <44383825+kubalaCEV@users.noreply.github.com>
2020-04-28 05:30:26 +0200

    CreateRutes
    Añadido Creación de rutas

--- app/src/main/java/com/example/grupob/beroutes/Home/HomeActivity.java
+++ app/src/main/java/com/example/grupob/beroutes/Home/HomeActivity.java
@@ -2,13 +2,12 @@ package com.example.grupob.beroutes.Home;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.grupob.beroutes.R;
 import com.example.grupob.beroutes.Utils.BottomNavigationViewHelper;
